19bef7c4811d9d2687ad22dd30429a3c9945c001
Pavlo Maksymchuk <pavlo.maksymchuk@yclip.co.uk>
2016-12-01 22:25:47 +0000

    initial greetings

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -33,14 +33,41 @@ exports.handle = (client) => {
     }
   })
 
+  const handleGreeting = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addResponse('greeting')
+      client.done()
+    }
+  })
+
+  const handleGoodbye = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt() {
+      client.addResponse('goodbye')
+      client.done()
+    }
+  })
+
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
+      // Add a greeting handler with a reference to the greeting stream
+      greeting: 'greeting',
+      goodbye: 'goodbye'
     },
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
       end: [untrained],
